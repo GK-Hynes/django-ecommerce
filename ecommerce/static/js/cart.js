80d4325af9678bcb00abd262dcc6024e076dcfed
Gerard Hynes <gkhynes@gmail.com>
2021-05-21 22:16:32 +0100

    Add updateItem view

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,36 @@
+const updateBtns = document.querySelectorAll(".update-cart")
+
+for (i = 0; i < updateBtns.length; i++){
+  updateBtns[i].addEventListener("click", function(){
+    const productId = this.dataset.product
+    const action = this.dataset.action
+    console.log('productId:', productId, 'Action:', action)
+
+    console.log('USER:', user)
+    if(user === 'AnonymousUSer'){
+      console.log('User is not authenticated')
+    } else {
+      updateUserOrder(productId, action)
+    }
+  })
+}
+
+function updateUserOrder(productId, action){
+  console.log('User is authenticated, sending data...')
+
+  const url = '/update_item/'
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    bosy: JSON.stringify({'productId': productId, 'action': action})
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    console.log('Data:', data)
+  })
+}
