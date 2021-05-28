42e451ac04f383119f369aadc48fdf7a55c4e761
Gerard Hynes <gkhynes@gmail.com>
2021-05-28 21:22:49 +0100

    Save cart to cookie for guest checkout

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,37 +1,59 @@
-const updateBtns = document.querySelectorAll(".update-cart")
+const updateBtns = document.querySelectorAll(".update-cart");
 
-for (i = 0; i < updateBtns.length; i++){
-  updateBtns[i].addEventListener("click", function(){
-    const productId = this.dataset.product
-    const action = this.dataset.action
-    console.log('productId:', productId, 'Action:', action)
+for (i = 0; i < updateBtns.length; i++) {
+  updateBtns[i].addEventListener("click", function () {
+    const productId = this.dataset.product;
+    const action = this.dataset.action;
+    console.log("productId:", productId, "Action:", action);
 
-    console.log('USER:', user)
-    if(user === 'AnonymousUSer'){
-      console.log('User is not authenticated')
+    console.log("USER:", user);
+    if (user === "AnonymousUSer") {
+      addCookieItem(productId, action);
     } else {
-      updateUserOrder(productId, action)
+      updateUserOrder(productId, action);
     }
-  })
+  });
 }
 
-function updateUserOrder(productId, action){
-  console.log('User is authenticated, sending data...')
+function addCookieItem(productId, action) {
+  console.log("User is not authenticated...");
+  if (action == "add") {
+    if (cart[productId] === undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
 
-  const url = '/update_item/'
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("Item should be deleted");
+      delete cart[productId];
+    }
+  }
+  console.log("Cart", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+}
+
+function updateUserOrder(productId, action) {
+  console.log("User is authenticated, sending data...");
+
+  const url = "/update_item/";
 
   fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken': csrftoken
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken
     },
-    body: JSON.stringify({'productId': productId, 'action': action})
+    body: JSON.stringify({ productId: productId, action: action })
   })
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    location.reload()
-  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      location.reload();
+    });
 }
